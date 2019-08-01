e051ddf31cecf4b203151c9dd0c98cf0c99bffc2
Rick Voorneveld <r.voorneveld@hoteliers.com>
2019-08-01 18:15:00 +0200

    Added highest z-index to place modal on top of all elements

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -1,4 +1,4 @@
-<div id="{{ $name }}" class="overlay fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center" style="background-color: rgba(0, 0, 0, .75)">
+<div id="{{ $name }}" class="overlay fixed z-50 top-0 right-0 bottom-0 left-0 flex items-center justify-center" style="background-color: rgba(0, 0, 0, .75)">
     <a href="#" class="absolute w-full h-full cursor-default"></a>
     <div class="relative w-full h-full md:h-auto md:w-3/4 lg:w-1/2 max-w-3xl p-6 lg:p-12 bg-highlight">
         {{ $slot }}
